1b47a6edb78f2ec528870535f04179152782a4dd
wangxiao0405 <1353052193@qq.com>
2018-08-27 21:20:10 +0800

    wx

--- js/bag2.js
+++ js/bag2.js
@@ -1,9 +1,22 @@
 window.onload=function () {
+    let beginlis=document.querySelectorAll(".begin ul li");
+    let xxkBox=document.querySelectorAll(".xxkBox");
     let navlis=document.querySelectorAll("nav li");
     let drop=document.querySelectorAll(".drop");
-    let dark=document.querySelectorAll(".dark");
+    console.log(beginlis, xxkBox,navlis,drop);
+    for (let i=0;i<beginlis.length;i++) {
+        beginlis[i].onclick=function () {
+            for (let j=0;j<beginlis.length;j++) {
+                xxkBox[j].style.zIndex="5";
+                beginlis[j].style.color="#3bacfe";
+                beginlis[j].style.background="#fafafa";
+            }
+            xxkBox[i].style.zIndex="10";
+            beginlis[i].style.color="#fff";
+            beginlis[i].style.background="#3bacfe"
+        }
+    }
 
-    // console.log(drop, navlis,dark,droplis);
     for (let i=0;i<navlis.length;i++) {
         navlis[i].onclick=function () {
             for(let j=0;j<navlis.length;j++){
@@ -14,8 +27,9 @@ window.onload=function () {
             drop[i].style.display="block";
             drop[i].style.zIndex="10";
             navlis[i].style.color="#36d681";
+        }
     }
-    }
+
     let droplis1=document.querySelectorAll(".drop1 li");
     dropOnclik(droplis1);
     let droplis2=document.querySelectorAll(".drop2 li");
@@ -25,9 +39,8 @@ window.onload=function () {
     function dropOnclik(droplisflag) {
         for(let i=0;i<droplisflag.length;i++){
             droplisflag[i].onclick=function () {
-                window.open("tap2.html");
+                window.open("bag2.html");
             }
         }
     }
-
-};
+}
